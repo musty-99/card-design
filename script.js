17e92d1feb4d61e894ffc45f2e71a364f204db7c
Mustapha Ibrahim <mustalex1@gmail.com>
2024-11-12 10:27:09 +0100

    nft-design

---
+++ script.js
@@ -0,0 +1,6 @@
+function myBio(obj) {
+    console.log(obj.name + " is " + obj.age + " years old and he is" + obj.student + " is a student " + " He loves " + obj.hobbies + "Reading Coding & Traveling");
+  }
+  
+   myBio= (obj.name); 
+
